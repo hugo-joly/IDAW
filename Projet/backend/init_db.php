76ef89cfbdab349207fc1be7b43e84989556cb97
baptiste <86311564+Aflagous@users.noreply.github.com>
2023-10-27 15:00:24 +0200

    wip

--- Projet/backend/init_db.php
+++ Projet/backend/init_db.php
@@ -1,22 +1,13 @@
 
 <?php
     require_once('config.php');
-
-    
     $dbName = "projet";
-
     $pdo->exec("DROP DATABASE IF EXISTS $dbName");
-
     $pdo->exec("CREATE DATABASE $dbName");
-
     $pdo->exec("USE $dbName");
-
     $sqlFile = 'sql/create_db.sql';
-    
     $sql = file_get_contents($sqlFile);
-    
     $pdo->exec($sql);
-    
     echo "Initialisation de la base de données réussie.";
-
 ?>
+
